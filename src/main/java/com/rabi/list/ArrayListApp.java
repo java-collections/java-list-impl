fd76f1111fbff1f9ff4a941b1a9f251d2de12cfa
rkpatra20 <rabindra@monamitech.com>
2018-08-30 20:55:00 +0530

    blank implementation for remove

--- src/main/java/com/rabi/list/ArrayListApp.java
+++ src/main/java/com/rabi/list/ArrayListApp.java
@@ -45,7 +45,8 @@ public class ArrayListApp implements IArrayList{
 	@Override
 	public void removeElementsFromArrayListExample() {
 		// TODO Auto-generated method stub
-		
+		// added blank body
+		// developers will write own code 
 	}
 	
 	
