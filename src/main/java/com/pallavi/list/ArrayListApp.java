0543e308a2ed917976dca49041c8fc21944b0c44
rkpatra20 <rabindra@monamitech.com>
2018-08-30 13:19:50 +0530

    initial commit

---
+++ src/main/java/com/pallavi/list/ArrayListApp.java
@@ -0,0 +1,13 @@
+package com.pallavi.list;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArrayListApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
