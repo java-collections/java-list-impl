b3522a5e911e0615d46101b48d1498126f305c2c
rkpatra20 <rabindra@monamitech.com>
2018-08-30 20:56:37 +0530

    formatted code

--- src/main/java/com/pallavi/list/ArrayListApp.java
+++ src/main/java/com/pallavi/list/ArrayListApp.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 import com.java.common.IArrayList;
 
-public class ArrayListApp implements IArrayList{
+public class ArrayListApp implements IArrayList {
 	public static void main(String[] args) {
-		IArrayList aa=new ArrayListApp();
+		IArrayList aa = new ArrayListApp();
 		aa.createArrayListExample();
 		aa.accessElementsFromArrayListExample();
 		aa.createArrayListFromCollectionExample();
-		
+
 	}
 
 	public void createArrayListExample() {
 		// TODO Auto-generated method stub
-		List<String> ll=new ArrayList();
+		List<String> ll = new ArrayList();
 		ll.add("Anu");
 		ll.add("Ammu");
 		ll.add("Anju");
@@ -28,18 +28,18 @@ public class ArrayListApp implements IArrayList{
 		ll.add("AAradhya");
 		ll.add("Anamika");
 		System.out.println(ll);
-		ll.add(4,"Ramesh");
+		ll.add(4, "Ramesh");
 		System.out.println(ll);
 	}
 
 	public void createArrayListFromCollectionExample() {
 		// TODO Auto-generated method stub
-		List <Integer> aa=new ArrayList();
+		List<Integer> aa = new ArrayList();
 		aa.add(2);
 		aa.add(3);
 		aa.add(4);
 		aa.add(5);
-		List <Integer> aa1=new ArrayList();
+		List<Integer> aa1 = new ArrayList();
 		aa1.add(6);
 		aa1.add(7);
 		aa1.add(8);
@@ -48,12 +48,12 @@ public class ArrayListApp implements IArrayList{
 		aa1.add(7);
 		aa.addAll(aa1);
 		System.out.println(aa1);
-		
+
 	}
 
 	public void accessElementsFromArrayListExample() {
 		// TODO Auto-generated method stub
-		List <String> aa2=new ArrayList<String>();
+		List<String> aa2 = new ArrayList<String>();
 		System.out.println(aa2.isEmpty());
 		aa2.add("Binita");
 		aa2.add("Bikash");
@@ -62,15 +62,15 @@ public class ArrayListApp implements IArrayList{
 		aa2.add("Binash");
 		aa2.add("Batash");
 		aa2.add("Badal");
-	System.out.println(aa2.size());	
-	System.out.println(aa2);
-	String firstName=aa2.get(0);
-	String secondName=aa2.get(1);
-	String lastName=aa2.get(aa2.size()-1);
-	System.out.println("firstName is"+firstName);
-	System.out.println("SecondName is"+secondName);
-	System.out.println("Last Name is"+lastName);
-	aa2.set(4, "pallavi");
-	System.out.println(aa2);
+		System.out.println(aa2.size());
+		System.out.println(aa2);
+		String firstName = aa2.get(0);
+		String secondName = aa2.get(1);
+		String lastName = aa2.get(aa2.size() - 1);
+		System.out.println("firstName is" + firstName);
+		System.out.println("SecondName is" + secondName);
+		System.out.println("Last Name is" + lastName);
+		aa2.set(4, "pallavi");
+		System.out.println(aa2);
 	}
 }
