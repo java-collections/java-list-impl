ceed58e01ee75cd3a0a0116da41422c50180182d
rkpatra20 <rabindra@monamitech.com>
2018-08-30 13:45:56 +0530

    added implementations

--- src/main/java/com/rabi/list/ArrayListApp.java
+++ src/main/java/com/rabi/list/ArrayListApp.java
@@ -1,10 +1,47 @@
 package com.rabi.list;
 
-public class ArrayListApp {
+import java.util.ArrayList;
+import java.util.List;
+
+import com.java.common.IArrayList;
+
+public class ArrayListApp implements IArrayList{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		IArrayList list=new ArrayListApp();
+		list.createArrayListExample();
 	}
+	
+	public  void createArrayListExample()
+	{
+		List<String> animals=new ArrayList<>();
+		animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Cat");
+        animals.add("Dog");
+
+        System.out.println(animals);
+
+        // Adding an element at a particular index in an ArrayList
+        animals.add(2, "Elephant");
+
+        System.out.println(animals);
+	}
+
+	@Override
+	public void createArrayListFromCollectionExample() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accessElementsFromArrayListExample() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
